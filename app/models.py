5dc1374cfee322ff8f55c3a09d29968711b90273
Tayper <polyakov_ks>
2024-09-11 10:26:59 +0500

    Поправил модели и добавил контекст для shell

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-    post: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
 
     def __repr__(self) -> str:
         return f"<User {self.username}>"
@@ -19,8 +19,8 @@ class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
     timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
-    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('user.id'), index=True)
-    author: so.Mapped[str] = so.relationship(back_populates='posts')
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
 
     def __repr__(self) -> str:
         return f"<Post {self.body}>"
